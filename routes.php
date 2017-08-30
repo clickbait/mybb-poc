b254da1213e53e5081349012155e672f5a2beafe
Neko <cronhound@gmail.com>
2017-08-30 18:57:40 +0930

    mapped the most important pages to routes

--- routes.php
+++ routes.php
@@ -7,3 +7,25 @@ $router->get( '/', function() {
 $router->get( '/members', function() {
 	get_page( 'memberlist' );
 });
+
+$router->get( '/latest', function() {
+	$_GET['action'] = 'getdaily';
+	get_page( 'search' );
+});
+
+$router->get( '/category/(\d+)', function($id) {
+	$_GET['fid'] = $id;
+	get_page( 'forumdisplay' );
+});
+
+$router->get( '/topic/(\d+)', function($id) {
+	$_GET['tid'] = $id;
+	get_page( 'showthread' );
+});
+
+$router->get( '/user/(\d+)', function($id) {
+	$_GET['action'] = 'profile';
+	$_GET['uid'] = $id;
+	get_page( 'member' );
+});
+
